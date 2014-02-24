54a6349f0c87e63cc5dd26a5f758bfe69ebd384a
Karsten Knese <karsten.knese@googlemail.com>
2014-02-24 09:52:40 +0100

    updated signalhelper

---
+++ include/dynamic-graph/TaskVelocityDamping/SignalHelper.h
@@ -0,0 +1,52 @@
+#ifndef SIGNALHELPER_H
+#define SIGNALHELPER_H
+
+#include <boost/shared_ptr.hpp>
+#include <dynamic-graph/all-signals.h>
+#include <dynamic-graph/linear-algebra.h>
+#include <dynamic-graph/all-commands.h>
+#include <dynamic-graph/factory.h>
+
+namespace dynamicgraph{
+
+namespace SignalHelper{
+
+typedef SignalTimeDependent < sot::MatrixHomogeneous, int > SignalTimeMatrix;
+typedef SignalTimeDependent < dynamicgraph::Vector, int > SignalTimeVector;
+typedef SignalTimeDependent < double, int > SignalTimeDouble;
+typedef SignalPtr<dynamicgraph::Vector, int > SignalPtrVector;
+typedef SignalPtr<dynamicgraph::Matrix, int > SignalPtrMatrix;
+
+inline boost::shared_ptr<SignalTimeDouble> createOutputSignalTimeDouble(std::string name){
+
+    std::string signal_name = "TaskVelocityDamping(taskDynamicVelocityvelDamp)::output(double)::"+name;
+    boost::shared_ptr<SignalTimeDouble> signal =  boost::shared_ptr<SignalTimeDouble>(
+                new SignalTimeDouble(NULL, signal_name));
+    return signal;
+}
+
+inline boost::shared_ptr<SignalTimeVector> createOutputSignalTimeVector(std::string name){
+
+    std::string signal_name = "TaskVelocityDamping(taskDynamicVelocityvelDamp)::output(vector)::"+name;
+    boost::shared_ptr<SignalTimeVector> signal =  boost::shared_ptr<SignalTimeVector>(
+                new SignalTimeVector(NULL, signal_name));
+    return signal;
+}
+
+inline boost::shared_ptr<SignalPtrMatrix> createInputSignalMatrix(std::string name){
+    std::string signal_name = "TaskVelocityDamping(taskDynamicVelocityvelDamp)::input(matrix)::"+name;
+    boost::shared_ptr<SignalPtrMatrix> signal = boost::shared_ptr<SignalPtrMatrix> (
+                new SignalPtrMatrix(NULL, signal_name));
+    return signal;
+}
+
+inline boost::shared_ptr<SignalPtrVector> createInputSignalVector(std::string name){
+    std::string signal_name = "TaskVelocityDamping(taskDynamicVelocityvelDamp)::input(vector)::"+name;
+    boost::shared_ptr<SignalPtrVector> signal = boost::shared_ptr<SignalPtrVector> (
+                new SignalPtrVector(NULL, signal_name));
+    return signal;
+}
+
+}
+}
+#endif
---
+++ src/dynamic_graph/sot/core/meta_task_dynamic_velocity_damping.py
@@ -0,0 +1,47 @@
+from dynamic_graph.sot.core.meta_task_6d import MetaTask6d,toFlags
+from dynamic_graph.sot.core.task_velocity_damping import TaskVelocityDamping
+
+from dynamic_graph import plug
+from dynamic_graph.sot.core import *
+
+from matrix_util import matrixToTuple
+
+class MetaTaskDynamicVelocityDamping(object):
+    name=''
+    dyn=0
+    task=0       
+    _ds = 0
+    _di = 0
+    controlGain = 0
+    
+    def createTask(self):
+        self.task = TaskVelocityDamping(self.name)
+        
+    def createOpPoint(self,opPoint,opPointRef = 'right-wrist'):
+        self.opPoint=opPoint
+        if self.opPointExist(opPoint): 
+            print 'no op point created in velocity damping'
+            return
+        self.dyn.createOpPoint(opPoint,opPointRef)
+        
+    def opPointExist(self,opPoint):
+        sigsP = filter(lambda x: x.getName().split(':')[-1] == opPoint,
+                       self.dyn.signals())
+        sigsJ = filter(lambda x: x.getName().split(':')[-1] == 'J'+opPoint,
+                       self.dyn.signals())
+        return len(sigsP)==1 & len(sigsJ)==1
+
+    def plugEverything(self):
+        self.task.di.value = self._di
+        self.task.ds.value = self._ds
+        self.task.controlGain.value = self.controlGain
+        self.task.dt.value = 0.001
+        
+    def __init__(self,name, di, ds, controlGain=1):
+        self.name='taskDynamicVelocity'+str(name)
+        self._ds = ds
+        self._di = di
+        self.controlGain = controlGain
+        self.createTask()
+        self.plugEverything()
+        
